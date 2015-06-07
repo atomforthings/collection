639f13c446ec34cced1451a499369f74f6fb59fb
neerajsohal <hello@neerajkumar.name>
2015-06-07 23:26:20 +0530

    purged code

--- src/Atom/Support/CollectionInterface.php
+++ src/Atom/Support/CollectionInterface.php
@@ -2,14 +2,6 @@
 
 namespace Atom\Support;
 
-use Countable;
-use ArrayAccess;
-use ArrayIterator;
-use CachingIterator;
-use JsonSerializable;
-use IteratorAggregate;
-use InvalidArgumentException;
-
-interface Collection implements ArrayAccess, Countable, IteratorAggregate, JsonSerializable {
-	;
+interface CollectionInterface {
+	
 }
