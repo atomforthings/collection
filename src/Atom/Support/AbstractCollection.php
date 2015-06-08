177a8ebcb299de0e8a7d8924362498291278c8d1
Scrutinizer Auto-Fixer <auto-fixer@scrutinizer-ci.com>
2015-06-08 08:36:38 +0000

    Scrutinizer Auto-Fixes
    This commit consists of patches automatically generated for this project on https://scrutinizer-ci.com

--- src/Atom/Support/AbstractCollection.php
+++ src/Atom/Support/AbstractCollection.php
@@ -17,8 +17,8 @@ abstract class AbstractCollection implements CollectionInterface {
 	}
 
 	public function getIterator() {
-        return new ArrayIterator($this->items);
-    }
+		return new ArrayIterator($this->items);
+	}
 
 	public function offsetExists($offset) {
 		return isset($this->items[$offset]);
@@ -30,7 +30,7 @@ abstract class AbstractCollection implements CollectionInterface {
 
 	public function offsetSet($offset, $value) {
 		if (is_null($offset)) $this->items[] = $value;
-        else $this->items[$offset] = $value;
+		else $this->items[$offset] = $value;
 	}
 
 	public function offsetUnset($offset) {
